2d49ae43415742d38eb6af7608573a1bc7507ee3
Papri Bhattacharjee <bhpapri03@gmail.com>
2025-09-07 08:14:13 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,32 @@
+const countryList = {
+    USD: "US", // US Dollar
+    INR: "IN", // Indian Rupee
+    EUR: "EU", // Euro
+    GBP: "GB", // British Pound
+    AUD: "AU", // Australian Dollar
+    CAD: "CA", // Canadian Dollar
+    JPY: "JP", // Japanese Yen
+    CNY: "CN", // Chinese Yuan
+    NZD: "NZ", // New Zealand Dollar
+    CHF: "CH", // Swiss Franc
+    SGD: "SG", // Singapore Dollar
+    HKD: "HK", // Hong Kong Dollar
+    SEK: "SE", // Swedish Krona
+    NOK: "NO", // Norwegian Krone
+    DKK: "DK", // Danish Krone
+    ZAR: "ZA", // South African Rand
+    AED: "AE", // UAE Dirham
+    SAR: "SA", // Saudi Riyal
+    THB: "TH", // Thai Baht
+    KRW: "KR", // South Korean Won
+    MYR: "MY", // Malaysian Ringgit
+    IDR: "ID", // Indonesian Rupiah
+    PHP: "PH", // Philippine Peso
+    BRL: "BR", // Brazilian Real
+    MXN: "MX", // Mexican Peso
+    RUB: "RU", // Russian Ruble
+    TRY: "TR", // Turkish Lira
+    PLN: "PL", // Polish Zloty
+    HUF: "HU", // Hungarian Forint
+    CZK: "CZ"  // Czech Koruna
+};
